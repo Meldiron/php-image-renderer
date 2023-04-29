f5fbb3f0bf5a6209fbdf719929f32e93d91c3814
Matej Bačo <matejbaco2000@gmail.com>
2023-04-29 19:07:03 +0200

    Init commit

---
+++ Dockerfile
@@ -0,0 +1,28 @@
+FROM php:8-apache
+
+# Composer
+COPY --from=composer:latest /usr/bin/composer /usr/local/bin/composer
+
+# Imagick
+RUN apt-get update && apt-get install -y libmagickwand-dev --no-install-recommends && rm -rf /var/lib/apt/lists/*
+RUN mkdir -p /usr/src/php/ext/imagick; \
+    curl -fsSL https://github.com/Imagick/imagick/archive/06116aa24b76edaf6b1693198f79e6c295eda8a9.tar.gz | tar xvz -C "/usr/src/php/ext/imagick" --strip 1; \
+    docker-php-ext-install imagick;
+RUN apt-get autoclean
+
+# CURL
+RUN apt-get update && apt-get install -y \
+    libcurl4-openssl-dev \
+    && docker-php-ext-install curl \
+    && a2enmod rewrite
+
+# Source
+COPY ./ /var/www/html/
+COPY ./fonts /var/www/html/fonts/
+RUN chmod -R 777 /var/www/html
+
+# Dependencies
+RUN composer install --no-dev --profile --ignore-platform-reqs
+
+# Startup
+CMD ["apache2-foreground"]
---
+++ index.php
@@ -0,0 +1,101 @@
+<?php
+
+error_reporting(E_ALL);
+ini_set('display_errors', 1);
+date_default_timezone_set('Europe/Prague');
+
+// 3004 = 30.4
+$dt = new DateTime('tomorrow');
+
+$d = $_REQUEST['d'] ?? '';
+if(!empty($d)) {
+    $month = substr($d, 0, 2);
+    $day = substr($d, 2, 2);
+    $dt = new DateTime($dt->format('o') . '-' . $month . '-' . $day);
+}
+
+$date = $dt->format('dm');
+
+$weekDay = $dt->format('N');
+$weekDayVerbose = match ($weekDay) {
+    '1' => 'pondělí',
+    '2' => 'úterý',
+    '3' => 'středa',
+    '4' => 'čtvrtek',
+    '5' => 'pátek',
+    '6' => 'sobota',
+    '7' => 'neděle',
+    default => ''
+};
+
+$dayVerbose = $dt->format('d.m.');
+$yearVerbose = $dt->format('o');
+
+$getNameDay = function() use ($date) {
+    $curl = curl_init('https://svatky.adresa.info/json?date=' . $date . '&lang=cs');
+    curl_setopt($curl, CURLOPT_RETURNTRANSFER, true);
+    $response = curl_exec($curl);
+    curl_close($curl);
+
+    $json = json_decode($response, true);
+    return $json[0]['name'] ?? '';
+};
+
+$name = $getNameDay();
+
+$image = new Imagick('bg.png');
+
+if(!empty($name)) {
+    $draw = new ImagickDraw();
+    $draw->setFont('fonts/Inter/Inter-SemiBold.ttf');
+    $draw->setFontSize(68);
+    $draw->setFontWeight(600);
+    $draw->setFillColor('#FFB9CC');
+    $draw->setTextAlignment(Imagick::ALIGN_CENTER);
+    $text = "SVÁTEK MÁ " . mb_strtoupper($name);
+    
+    $image->annotateImage($draw, $image->getImageWidth() /2, 1110, 0, $text);
+}
+
+if(!empty($weekDayVerbose)) {
+    $draw = new ImagickDraw();
+    $draw->setFont('fonts/Inter/Inter-Bold.ttf');
+    $draw->setFontSize(64);
+    $draw->setFontWeight(700);
+    $draw->setTextKerning(5);
+    $draw->setFillColor('#C5C5C5');
+    $draw->setTextAlignment(Imagick::ALIGN_LEFT);
+    $text = mb_strtoupper($weekDayVerbose);
+    
+    $image->annotateImage($draw, 96, 250, 0, $text);
+}
+
+// Day, Month
+$draw = new ImagickDraw();
+$draw->setFont('fonts/Poppins/Poppins-Bold.ttf');
+$draw->setFontSize(56);
+$draw->setFontWeight(700);
+$draw->setTextKerning(10);
+$draw->setFillColor('#FFFFFF');
+$draw->setTextAlignment(Imagick::ALIGN_LEFT);
+$text = $dayVerbose;
+$image->annotateImage($draw, 204, 160, 0, $text);
+
+// Year 
+$metrics = $image->queryFontMetrics($draw, $text);
+$draw = new ImagickDraw();
+$draw->setFont('fonts/Poppins/Poppins-Medium.ttf');
+$draw->setFontSize(56);
+$draw->setFontWeight(400);
+$draw->setTextKerning(10);
+$draw->setFillColor('#FFFFFF');
+$draw->setTextAlignment(Imagick::ALIGN_LEFT);
+$text = $yearVerbose;
+$image->annotateImage($draw, 204 + $metrics['textWidth'] + 14, 160, 0, $text);
+
+
+header('Content-Type: image/jpeg');
+
+echo $image;
+
+$image->destroy();
